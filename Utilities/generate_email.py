ebf3778bda51efd36fc0a2154e9c09b5debca37b
shubham <shubham.vijayvargiya@arcgate.com>
2023-12-12 19:30:48 +0530

    new commit

--- Utilities/generate_email.py
+++ Utilities/generate_email.py
@@ -1,4 +1,6 @@
 import time
+import string
+import random
 
 
 def generate_email_time_stamp():
@@ -10,3 +12,8 @@ def generate_username_time_stamp():
     timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
     timestamp = timestamp.replace('-', '_').replace(' ', '_').replace(':', '_')
     return "arun"+timestamp
+
+def generate_random_string_subject():
+    number = 7
+    result = ''.join(random.choices(string.ascii_lowercase, k=number))
+    return result
