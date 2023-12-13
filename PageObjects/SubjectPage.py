ef72f4e4d5f31b4a00771a49f1b9f43f5ccf4b74
shubham <shubham.vijayvargiya@arcgate.com>
2023-12-13 19:18:30 +0530

    latest commit

--- PageObjects/SubjectPage.py
+++ PageObjects/SubjectPage.py
@@ -21,6 +21,7 @@ class Subject:
         self.total_row_count_of_table_xpath = 'tbody tr'
         self.click_on_cancel_button_xpath = 'close-button'
         self.click_on_delete_button_xpath = 'Delete'
+        self.click_on_edit_button_xpath = 'span.editButton'
         self.get_text_on_table_of_subject_xpath = '//*[@id="demo"]/tr[1]/td[1]'
         
     def click_on_add_new_subject_button(self):
@@ -49,6 +50,9 @@ class Subject:
     def click_on_delete_button(self):
         self.driver.find_element(By.ID, self.click_on_delete_button_xpath).click()
         
+    def click_on_edit_button(self):
+        self.driver.find_element(By.CSS_SELECTOR, self.click_on_edit_button_xpath).click()
+        
     def get_text_on_table_of_subject(self):
         element = self.driver.find_element(By.XPATH, self.get_text_on_table_of_subject_xpath).text
         return element
@@ -57,18 +61,19 @@ class Subject:
         time.sleep(2)
         self.driver.find_element(By.ID, self.click_on_cancel_button_xpath).click()
         
-    def fill_the_form_add_new_subject(self,subject_data):
+    def section_open_of_subject_section(self):
         login = Login(self.driver)
         login.fill_username_password_input(config.get("crediential","login_username"), config.get("crediential","login_password"))
         login.click_on_add_subject_section()
+        
+    def fill_the_form_add_new_subject(self,subject_data):
+        self.section_open_of_subject_section()
         self.click_on_add_new_subject_button()
         self.add_new_subject_input_type(subject_data)
         self.new_subject_add_save_button()
         
     def not_save_new_subject_click_on_cancel_button(self):
-        login = Login(self.driver)
-        login.fill_username_password_input(config.get("crediential","login_username"), config.get("crediential","login_password"))
-        login.click_on_add_subject_section()
+        self.section_open_of_subject_section()
         old_row = 0
         for old_row_data in self.row_count_of_subject_table():
             old_row = old_row + 1
@@ -83,9 +88,7 @@ class Subject:
             assert False
             
     def delete_row_in_the_subject_table(self):
-        login = Login(self.driver)
-        login.fill_username_password_input(config.get("crediential","login_username"), config.get("crediential","login_password"))
-        login.click_on_add_subject_section()
+        self.section_open_of_subject_section()
         self.click_on_delete_button()
         self.driver.switch_to.alert.accept()
         if 'Subject deleted successfully' in self.display_message_respect_of_subject():
@@ -94,15 +97,58 @@ class Subject:
             assert False
             
     def not_delete_row_in_subject_table(self, screenshot):
-        login = Login(self.driver)
-        login.fill_username_password_input(config.get("crediential","login_username"), config.get("crediential","login_password"))
-        login.click_on_add_subject_section()
+        self.section_open_of_subject_section()
         element = self.get_text_on_table_of_subject()
         self.click_on_delete_button()
         time.sleep(3)
         self.driver.switch_to.alert.dismiss()
         data = self.get_text_on_table_of_subject()
         if element == data:
+            assert True
+        else:
+            self.driver.save_screenshot(screenshot)
+            assert False
+
+    def update_existing_data_in_the_subject_table_without_change(self, screenshot):
+        self.section_open_of_subject_section()
+        previous_subject = self.get_text_on_table_of_subject()
+        self.click_on_edit_button()
+        self.click_on_cancel_button()
+        time.sleep(2)
+        new_subject = self.get_text_on_table_of_subject()
+        if previous_subject == new_subject:
+            assert True
+        else:
+            self.driver.save_screenshot(screenshot)
+            assert False
+            
+    def update_existing_subject_click_on_edit_button(self, subject_data):
+        self.section_open_of_subject_section()
+        self.click_on_edit_button()
+        time.sleep(2)
+        if subject_data:
+            self.driver.find_element(By.NAME, self.add_new_subject_input_type_xpath).clear()
+            self.add_new_subject_input_type(subject_data)
+        else:
+            self.driver.find_element(By.NAME, self.add_new_subject_input_type_xpath).clear()
+        self.new_subject_add_save_button()
+
+    def validation_message_for_subject(self, screenshot):
+        if 'This field should start with a letter' in self.display_error_message_respect_of_parsley_pattern():
+            assert True
+        else:
+            self.driver.save_screenshot(screenshot)
+            assert False
+
+    def validation_message_for_required_subject(self, screenshot):
+        if 'This field is required.' in self.display_error_message_respect_of_subject():
+            assert True
+        else:
+            self.driver.save_screenshot(screenshot)
+            assert False
+            
+    def success_message_for_subject(self, message ,screenshot):
+        if message in self.display_message_respect_of_subject():
             assert True
         else:
             self.driver.save_screenshot(screenshot)
--- tests/test_subject.py
+++ tests/test_subject.py
@@ -18,104 +18,100 @@ class TestSubject(logclass):
     def test_add_new_subject_with_valid_data(self):
         log = self.getthelogs()
         log.info('TEST CASE, test_add_new_subject_with_valid_data')
-        log.info("Test Case Starting")
         subject = Subject(self.driver)
         subject.fill_the_form_add_new_subject(generate_random_string_subject())
-        if 'Subject added successfully' in subject.display_message_respect_of_subject():
-            assert True
-        else:
-            self.driver.save_screenshot('Screenshots/test_add_new_subject_with_valid_data.png')
-            assert False
+        subject.success_message_for_subject('Subject added successfully', 'Screenshots/test_add_new_subject_with_valid_data.png')
     
     def test_add_new_subject_without_data(self):
         log = self.getthelogs()
         log.info('TEST CASE, test_add_new_subject_without_data')
-        log.info("Test Case Starting")
         subject = Subject(self.driver)
         subject.fill_the_form_add_new_subject('')
-        if 'This field is required.' in subject.display_error_message_respect_of_subject():
-            assert True
-        else:
-            self.driver.save_screenshot('Screenshots/test_add_new_subject_without_data.png')
-            assert False
+        subject.validation_message_for_required_subject('Screenshots/test_add_new_subject_without_data.png')
             
     def test_add_new_subject_with_numeric_data(self):
         log = self.getthelogs()
         log.info('TEST CASE, test_add_new_subject_with_numeric_data')
-        log.info("Test Case Starting")
         subject = Subject(self.driver)
         subject.fill_the_form_add_new_subject('1234')
-        if "This field should start with a letter and doesn't accept digits" in subject.display_error_message_respect_of_parsley_pattern():
-            assert True
-        else:
-            self.driver.save_screenshot('Screenshots/test_add_new_subject_with_numeric_data.png')
-            assert False
+        subject.validation_message_for_subject('Screenshots/test_add_new_subject_with_numeric_data.png')
 
     def test_add_new_subject_with_special_character_data(self):
         log = self.getthelogs()
         log.info('TEST CASE, test_add_new_subject_with_special_character_data')
-        log.info("Test Case Starting")
         subject = Subject(self.driver)
         subject.fill_the_form_add_new_subject('####')
-        if "This field should start with a letter and doesn't accept digits" in subject.display_error_message_respect_of_parsley_pattern():
-            assert True
-        else:
-            self.driver.save_screenshot('Screenshots/test_add_new_subject_with_special_character_data.png')
-            assert False
+        subject.validation_message_for_subject('Screenshots/test_add_new_subject_with_special_character_data.png')
     
     def test_add_new_subject_with_numeric_data_along_with_string(self):
         log = self.getthelogs()
         log.info('TEST CASE, test_add_new_subject_with_numeric_data_along_with_string')
-        log.info("Test Case Starting")
         subject = Subject(self.driver)
         subject.fill_the_form_add_new_subject('shubha111')
-        if "This field should start with a letter and doesn't accept digits" in subject.display_error_message_respect_of_parsley_pattern():
-            assert True
-        else:
-            self.driver.save_screenshot('Screenshots/test_add_new_subject_with_numeric_data_along_with_string.png')
-            assert False
+        subject.validation_message_for_subject('Screenshots/test_add_new_subject_with_numeric_data_along_with_string.png')
     
     def test_add_new_subject_with_alterat_along_with_string(self):
         log = self.getthelogs()
         log.info('TEST CASE, test_add_new_subject_with_alterat_along_with_string')
-        log.info("Test Case Starting")
         subject = Subject(self.driver)
         subject.fill_the_form_add_new_subject('shubha@@@@')
-        if "This field should start with a letter and doesn't accept digits" in subject.display_error_message_respect_of_parsley_pattern():
-            assert True
-        else:
-            self.driver.save_screenshot('Screenshots/test_add_new_subject_with_alterat_along_with_string.png')
-            assert False
+        subject.validation_message_for_subject('Screenshots/test_add_new_subject_with_alterat_along_with_string.png')
             
     def test_add_new_subject_with_special_character_along_with_string(self):
         log = self.getthelogs()
         log.info('TEST CASE, test_add_new_subject_with_special_character_along_with_string')
-        log.info("Test Case Starting")
         subject = Subject(self.driver)
         subject.fill_the_form_add_new_subject('shubha!!!!')
-        if 'Subject added successfully' in subject.display_message_respect_of_subject():
-            assert True
-        else:
-            self.driver.save_screenshot('Screenshots/test_add_new_subject_with_special_character_along_with_string.png')
-            assert False
+        subject.success_message_for_subject('Subject added successfully', 'Screenshots/test_add_new_subject_with_special_character_along_with_string.png')
             
     def test_add_new_subject_with_add_new_subject_and_click_on_cancel_button(self):
         log = self.getthelogs()
         log.info('TEST CASE, test_add_new_subject_with_add_new_subject_and_click_on_cancel_button')
-        log.info("Test Case Starting")
         subject = Subject(self.driver)
         subject.not_save_new_subject_click_on_cancel_button()
         
     def test_successfully_deleted_row_in_the_subject_table(self):
         log = self.getthelogs()
         log.info('TEST CASE, test_successfully_deleted_row_in_the_subject_table')
-        log.info("Test Case Starting")
         subject = Subject(self.driver)
         subject.delete_row_in_the_subject_table()
         
     def test_not_deleted_row_from_subject_table(self):
         log = self.getthelogs()
         log.info('TEST CASE, test_not_deleted_row_from_subject_table')
-        log.info("Test Case Starting")
         subject = Subject(self.driver)
         subject.not_delete_row_in_subject_table('Screenshots/test_not_deleted_row_from_subject_table.png')
+
+    def test_existing_data_update_using_edit_button(self):
+        log = self.getthelogs()
+        log.info('TEST CASE, test_existing_data_update_using_edit_button')
+        subject = Subject(self.driver)
+        subject.update_existing_data_in_the_subject_table_without_change('Screenshots/test_existing_data_update_using_edit_button.png')
+        
+    def test_click_edit_button_and_then_subject_clear_and_click_save_button(self):
+        log = self.getthelogs()
+        log.info('TEST CASE, test_click_edit_button_and_then_subject_clear_and_click_save_button')
+        subject = Subject(self.driver)
+        subject.update_existing_subject_click_on_edit_button('')
+        subject.validation_message_for_required_subject('Screenshots/test_click_edit_button_and_then_subject_clear_and_click_save_button.png')
+    
+    def test_click_edit_button_and_then_subject_clear_and_change_subject_then_click_save_button(self):
+        log = self.getthelogs()
+        log.info('TEST CASE, test_click_edit_button_and_then_subject_clear_and_change_subject_then_click_save_button')
+        subject = Subject(self.driver)
+        subject.update_existing_subject_click_on_edit_button(generate_random_string_subject())
+        subject.success_message_for_subject('Subject updated successfully', 'Screenshots/test_click_edit_button_and_then_subject_clear_and_change_subject_then_click_save_button.png')
+            
+    def test_click_edit_button_and_then_subject_clear_and_change_subject_data_with_number_then_click_save_button(self):
+        log = self.getthelogs()
+        log.info('TEST CASE, test_click_edit_button_and_then_subject_clear_and_change_subject_data_with_number_then_click_save_button')
+        subject = Subject(self.driver)
+        subject.update_existing_subject_click_on_edit_button('asdfghj1')
+        subject.validation_message_for_subject('Screenshots/test_click_edit_button_and_then_subject_clear_and_change_subject_data_with_number_then_click_save_button.png')
+
+    def test_click_edit_button_and_then_subject_clear_and_change_subject_data_with_special_character_then_click_save_button(self):
+        log = self.getthelogs()
+        log.info('TEST CASE, test_click_edit_button_and_then_subject_clear_and_change_subject_data_with_special_character_then_click_save_button')
+        subject = Subject(self.driver)
+        subject.update_existing_subject_click_on_edit_button('asdfghj@')
+        subject.validation_message_for_subject('Screenshots/test_click_edit_button_and_then_subject_clear_and_change_subject_data_with_special_character_then_click_save_button.png')
